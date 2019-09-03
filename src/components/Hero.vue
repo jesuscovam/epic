c41f8e2cd6f186fbc349fca98b68e7043af1ec3a
jesus <jesuscovam@icloud.com>
2019-09-03 16:42:30 -0500

    changed epic tile to lowercase

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@
     <div class="columns">
       <div class="column">
         <div class="hero center">
-          <h1 class="title">Epic</h1>
+          <h1 class="title">epic</h1>
           <h2 class="subtitle">Reserva experiencias en la Riviera Maya</h2>
           <router-link to="encuesta">
             <button class=" button is-primary is-rounded">
